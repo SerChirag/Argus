7de5c6859c09bb6888639c3e0df46d46a13d814d
Prabhakar Prasad <prabhakarpd7284@gmail.com>
2019-04-05 13:25:44 -0230

    basic layout and routes

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ import logging
 from logging import Formatter, FileHandler
 from forms import *
 import os
-
+from flask import Flask, render_template, flash, request, jsonify
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+ 
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -36,6 +38,16 @@ def login_required(test):
             return redirect(url_for('login'))
     return wrap
 '''
+
+
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+    # email = TextField('Email:', validators=[validators.required(), validators.Length(min=6, max=35)])
+    # password = TextField('Password:', validators=[validators.required(), validators.Length(min=3, max=35)])
+ 
+
+
+
 #----------------------------------------------------------------------------#
 # Controllers.
 #----------------------------------------------------------------------------#
@@ -45,10 +57,79 @@ def login_required(test):
 def home():
     return render_template('pages/placeholder.home.html')
 
-@app.route('/20-20')
+response=[]
+counter=0
+image_dict={
+    'a_1.jpg':'a_2.jpg',
+    'a_2.jpg':'a_3.jpg',
+    'a_3.jpg':'a_4.jpg',
+    'a_4.jpg':'a_5.jpg',
+    'a_5.png':'a_6.jpg',
+    'a_6.jpg':'test.png',
+}
+spell_dict={
+    'a_1.jpg':'kvd',
+    'a_2.jpg':'zshc',
+    'a_3.jpg':'kvd',
+    'a_4.jpg':'kvd',
+    'a_5.jpg':'zshc',
+    'a_6.jpg':'kvd',
+    'test.png':'a_1.jpg',
+}
+
+def calculate():
+    sum=0
+    for j in response:
+        sum = sum+(int)(j)
+    return sum
+
+@app.route('/process', methods=['POST'])
+def process():
+    name=request.form['name']
+    img_name = request.form['hidden_img']
+    flag = (name == spell_dict[img_name])
+    # if (not flag):
+    #     return jsonify({'error':'ERROR'})
+    # flash('Error: All the form fields are required. ')
+    return jsonify({'name':image_dict[img_name], 'flag':flag})
+
+@app.route('/20-20', methods=['GET', 'POST'])
 def twenty():
-    form = LoginForm(request.form)
-    return render_template('pages/20-20.html', form=form)
+    # form = LoginForm(request.form)
+    # form = ReusableForm(request.form)
+    # print (form.errors)
+    # if request.method == 'POST':
+    #     name=request.form['name']
+    #     img_name = request.form['hidden_img']
+    #     response.append(name)
+        # counter = counter + 1
+        # password=request.form['password']
+        # email=request.form['email']
+        # print (name, " ", email, " ", password)
+    
+    # if form.validate():
+    #     # Save the comment here.
+    #     if(name=='10'):
+    #         flash('Thanks for registration ' + (str)(calculate()))
+    #     else:
+    #         flash('Thanks for registration '+image_dict[img_name])
+    # else:
+    #     flash('Error: All the form fields are required. ')
+    
+    # return render_template('hello.html', form=form)
+    return render_template('pages/20-20.html')
+
+@app.route('/color_vision', methods=['GET', 'POST'])
+def color_vision():
+    return render_template('pages/color_vision.html')
+
+@app.route('/side_vision', methods=['GET', 'POST'])
+def side_vision():
+    return render_template('pages/side_vision.html')
+
+@app.route('/eye_coordination', methods=['GET', 'POST'])
+def eye_coordination():
+    return render_template('pages/eye_coordination.html')
 
 @app.route('/about')
 def about():
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,39 @@
 (function() {
+$(document).ready(function() {
+    var x  = document.getElementById("cur_img").src.split('/')
+    document.getElementById("hidden_img").value = x[x.length-1]
+    
+    $('form').on('submit', function(event){
+        $.ajax({
+            data : {
+                name : $('#name').val(),
+                hidden_img : $('#hidden_img').val()
+            },
+            type : 'POST',
+            url : '/process'
+        })
+        .done(function(data){
+            if(data.error){
+                $('#errorAlert').text(data.error).show();
+                $('#successAlert').hide();
+            }
+            else{
+                console.log(data.name)
+                document.getElementById("cur_img").src='http://localhost:5000/static/img/'+data.name
+                document.getElementById("hidden_img").value = data.name
+                console.log(data.flag)
+                if(data.flag) {
+                    $('#successAlert').text(data.name).show();
+                    $('#errorAlert').hide();
+                }
+                else{
+                    $('#errorAlert').text(data.error).show();
+                    $('#successAlert').hide();
+                }
 
+            }
+        });
+        event.preventDefault();
+    });
+});
 }).call(this);
